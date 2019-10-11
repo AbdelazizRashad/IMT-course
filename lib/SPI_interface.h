af0fa65bf3d8c9985515fc18e8c585b02b24cbff
Eng/ Abdelaziz Rashad <47900302+AbdelazizRashad@users.noreply.github.com>
2019-10-11 19:50:13 +0200

    Update SPI_interface.h

--- lib/SPI_interface.h
+++ lib/SPI_interface.h
@@ -5,42 +5,32 @@
 #ifndef _SPI_INTERFACE_H
 #define _SPI_INTERFACE_H
 
-void SPI_vidInitMaster(void);
-void SPI_vidInitSlave(void);
-void SPI_vidEnableSS(void);
-void SPI_vidDisableSS(void);
-void SPI_vidTransferByte(u8);
-u8 SPI_u8ReceiveByte(void);
-void SPI_vidEnableInterrupt(void);
-void SPI_vidDisableInterrupt(void);
 
-/*Definitions of pins and ports*/
-#define SPI_MOSI_PORT DIO_PORTB
-#define SPI_MOSI_PIN DIO_PIN5
-#define SPI_MISO_PORT DIO_PORTB
-#define SPI_MISO_PIN DIO_PIN6
-#define SPI_SCK_PORT DIO_PORTB
-#define SPI_SCK_PIN DIO_PIN7
-#define SPI_SS_PORT DIO_PORTB
-#define SPI_SS_PIN DIO_PIN4
+// SPI Configurations
+#define SPI_prescaller			1	// prescaller[8]={4,16,64,128 , 2,8,32,64}
+#define SPI_polarity_phase		0	// polarity_phase[4]={(LRSR) , (LRSF) , (LFSF) , (LFSR)}
+// L>>leading , S>> sample, R>> Raising, F>> Falling
+#define SPI_interrupt_polling	0	// 1 for interr ,, 0 for polling
+#define SPI_data_ordered		1	//1 for LSB first send ,, 0 for MSB first send
 
-
-#ifdef ST_MC_ATMEGA128
-#define SPI_MOSI_PORT DIO_PORTB
-#define SPI_MOSI_PIN DIO_PIN2
-#define SPI_MISO_PORT DIO_PORTB
-#define SPI_MISO_PIN DIO_PIN3
-#define SPI_SCK_PORT DIO_PORTB
-#define SPI_SCK_PIN DIO_PIN1
-#define SPI_SS_PORT DIO_PORTB
-#define SPI_SS_PIN DIO_PIN0
-#endif
 
 #define SPI_CLK_RISING 0
 #define SPI_CLK_FALLING 1
 
 #define SPI_CLK_PHASE_SAMPLE 0
 #define SPI_CLK_PHASE_SETUP 1
+
+// functions modified by zizo
+void SPI_vidInitMaster(void);
+void SPI_vidInitSlave(void);
+u8 SPI_u8Tranciever(u8 u8data);
+void SPI_vidSend(u8 u8data);
+u8 SPI_u8Recieve(void);
+void SPI_vidEnableSS(void);
+void SPI_vidDisableSS(void);
+// will be discussed later
+void SPI_vidEnableInterrupt(void);
+void SPI_vidDisableInterrupt(void);
 
 #endif
 
